8f699bfc77c6072fa75907996a4705ddd5b4b758
MalinoweSchody <47105178+MalinoweSchody@users.noreply.github.com>
2019-01-28 11:56:28 +0100

    Add files via upload

---
+++ Kalkulator Założenia/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+
+	
+	
+	return 0;
+}
+
+
+
+//1.Odbieranie danych od u¿ytkownika, gdy bêdzie to wymagane
+
+//NIE ZROBIONE [MŒ]- 2.Dodawanie: sk³adnik + sk³adnik = suma
+
+//NIE ZROBIONE [MŒ]-3.Odejmowanie: Odjemna – odjemnik = ró¿nica
+
+//NIE ZROBIONE [K¯]-4.Mno¿enie: Czynnik · czynnik = iloczyn
+
+//NIE ZROBIONE [K¯]-5.Dzielenie: Dzielna : dzielnik = iloraz
+
+//NIE ZROBIONE [K¯]-6.Obliczanie procentów, u¿ytkownik bêdzie mia³ do wyboru ró¿ne funkcje takie jak:
+//a)Oblicz A % z B
+//b)A jest jakim procentem z B
+//c)O jaki procent wzros³a/zmala³a liczba? 
+//z A do B ?
+//d)Dodaj procent do liczby. 
+//A + B %
+//e)Odejmij procent od liczby. 
+//A - B %
+
+//NIE ZROBIONE [MŒ]-7.Potêgowanie – dzia³anie dwuargumentowe (n-ta potêga)
+
+//NIE ZROBIONE [K¯]-8.Silnia liczby naturalnej n
+
+//NIE ZROBIONE [MŒ]-9.Pierwiastek (n-ty pierwiastek)
+
+//10. Czytelne wypisywanie wyniku z funkcji dla u¿ytkownika
+
+//50% [MŒ] - 50% [K¯]
